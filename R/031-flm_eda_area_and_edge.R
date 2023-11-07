92fb83625778e55b85179be03d18f30588fe5f42
Andrade Velásquez Esteban Andrés <Andrade@iabg.de>
2023-11-07 16:49:18 +0100

    Tidied all area metrics + ggplot function

--- R/031-flm_eda_area_and_edge.R
+++ R/031-flm_eda_area_and_edge.R
@@ -7,10 +7,11 @@
 # lm_by_type <- list_lsm() |> group_by(type) |> nest()
 #
 
-# functions
-my_ggplot <- function(data, x, y, title){
+#Functions--------------------------------------------------------------------
+
+my_ggplot <- function(data, x, y, title, subtitle=NULL){
     ggplot(data) +
-        ggtitle(title) +
+        ggtitle(title, subtitle) +
         geom_bar(aes({{x}},
                      {{y}},
                      fill=class),
@@ -18,9 +19,14 @@ my_ggplot <- function(data, x, y, title){
 }
 
 
+#------------Percentage of landscape area -------------------------------
+# It is the percentage of the landscape belonging to class i.
+# It is a measure of composition and because of the relative character directly comparable
+# among landscapes with different total areas.
 
-##---------------------------------------------------------------------------
-# Class area and its percentage of total landscape
+##
+##NOTE: We include two pland calculations in this chapter. One with background ("manual" calculation)
+##      and one without (built-in metric)
 
 
 #Copy the sample_lsm output as xx and group by landscape extent name
@@ -40,7 +46,7 @@ aoi_w_area <- aoi |>
     st_drop_geometry()
 
 
-# Use the total landscape area to calculate class area percentage and plot
+# Do a left join to the dataset with total landscape area and define it as a new variable
 xx2 <- xx |>
     left_join(aoi_w_area) |>
     mutate(landscape_extent = plot_id,
@@ -49,70 +55,77 @@ xx2 <- xx |>
            class            = factor(class))
 
 
-# Use the total landscape area to calculate class area percentage and plot
-xx_area_plot <- xx |>
+# Use the total landscape area to calculate class area percentage (background considered)
+xx_pland_w_backg <- xx2 |>
     filter(metric == "ca") |>
-    ggplot() + ggtitle("Class area relative to landscape area") + geom_bar(aes(landscape_extent, relative_area,  fill = class), stat = "identity")
+    my_ggplot(landscape_extent,
+              relative_area,
+              "Class area relative to landscape area",
+              "With background" )
 
 
 
-# Calculate percentage of landscape (per class) using the built-in metric pland
-xx_pland <- xx2 |>
-    filter(metric=="pland", level == "class") |>
+# Calculate percentage of landscape per class using the built-in metric pland (background not considered)
+xx_pland_wout_backg  <- xx2 |>
+    filter(metric=="pland",
+           level == "class") |>
     mutate(percentage_of_land = value) |>
-    my_ggplot(landscape_extent, percentage_of_land, "title")
+    my_ggplot(landscape_extent,
+              percentage_of_land,
+              "Percentage of Land",
+              "No background")
 
 
 
-#----------------------------------------------------------------------------
-# Radius of gyration (class level)
-    # Radius of gyration is a measure of patch extent (i.e., how far-reaching it is);
-    # thus, it is effected by both patch size and patch compaction.
-    # Note that the choice of the 4-neighbor or 8-neighbor rule for delineating patches
-    # will have an impact on this metric.
-    # Behaviour: Approaches GYRATE_MN = 0 if every patch is a single cell. Increases, without limit, when only one patch is present.
+#------------Radius of gyration (class level)---------------------------------------------
+# Radius of gyration is a measure of patch extent (i.e., how far-reaching it is);
+# thus, it is effected by both patch size and patch compaction.
+# Note that the choice of the 4-neighbor or 8-neighbor rule for delineating patches
+# will have an impact on this metric.
+# Behaviour: Approaches GYRATE_MN = 0 if every patch is a single cell. Increases, without limit, when only one patch is present.
 
 
-xx_gyrate <- xx |>
-    filter(metric=="gyrate_mn", level == "class") |>
+xx_gyrate <- xx2 |>
+    filter(metric=="gyrate_mn",
+           level == "class") |>
     mutate(gyration_value = value) |>
-    ggplot() +
-    ggtitle("Radius of Gyration") +
-    geom_bar(aes(landscape_extent,
-                 gyration_value,
-                 fill=class),
-             stat="identity")
+    my_ggplot(landscape_extent,
+              gyration_value,
+              "Gyration value")
 
 
 
 
-#----------------------------------------------------------------------------
-# Largest patch index
-    # It is the percentage of the landscape covered by the corresponding largest patch of each class i.
-    # It is a simple measure of dominance.
+#------------Largest patch index---------------------------------------------------------
+# It is the percentage of the landscape covered by the corresponding largest patch of each class i.
+# It is a simple measure of dominance.
 
-xx_lpi <- xx |>
-    filter(metric=="lpi", level == "class") |>
-    mutate(plot_id, lpi = value, class=factor(class)) |>
-    ggplot() + ggtitle("Largest Patch Index") + geom_bar(aes(plot_id, lpi, fill=class), stat="identity")
-
-
-
-
-#----------------------------------------------------------------------------
-# Edge density
-    # The edge density equals the sum of all edges of class i in relation to the landscape area.
-    # The boundary of the landscape is only included in the corresponding total class edge length
-    # if count_boundary = TRUE. The metric describes the configuration of the landscape,
-    # e.g. because an aggregation of the same class will result in a low edge density.
-    # The metric is standardized to the total landscape area, and therefore comparisons among
-    # landscapes with different total areas are possible.
+xx_lpi <- xx2 |>
+    filter(metric=="lpi",
+           level == "class") |>
+    mutate(lpi = value) |>
+    my_ggplot(landscape_extent,
+              lpi,
+              "Largest Patch Index")
 
 
-xx_ed <- xx |>
-    filter(metric=="ed", level == "class") |>
-    mutate(landscape_extent = plot_id, ed = value, class=factor(class)) |>
-    ggplot() + ggtitle("Edge density") + geom_bar(aes(landscape_extent, ed, fill=class), stat="identity")
+
+#------------Edge density----------------------------------------------------------------
+# The edge density equals the sum of all edges of class i in relation to the landscape area.
+# The boundary of the landscape is only included in the corresponding total class edge length
+# if count_boundary = TRUE. The metric describes the configuration of the landscape,
+# e.g. because an aggregation of the same class will result in a low edge density.
+# The metric is standardized to the total landscape area, and therefore comparisons among
+# landscapes with different total areas are possible.
+
+
+xx_ed <- xx2 |>
+    filter(metric=="ed",
+           level == "class") |>
+    mutate(ed = value) |>
+    my_ggplot(landscape_extent,
+              ed,
+              "Edge density")
 
 
 
