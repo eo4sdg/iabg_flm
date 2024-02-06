1abb99112ddd044338e5d6f74485c36b7ca76721
Emin Dzhaner <Emin@iabg.de>
2024-02-06 10:25:59 +0100

    further testing

--- R/06_map_metrics.R
+++ R/06_map_metrics.R
@@ -1,10 +1,19 @@
 library(landscapemetrics)
 library(terra)
 
+
+
 # specify tempdir because next functions only write to
 # tempdir
-terraOptions(tempdir = "data/temp")
 
+tempdir <- "data/temp"
+terraOptions(tempdir = tempdir)
+fs::dir_create(tempdir)
+
+# nee the extra information later for joining
+types <-
+    list_lsm() |>
+    distinct(metric, .keep_all = TRUE)
 
 
 landscape <- "input raster with classes"
@@ -13,17 +22,46 @@ landscape <- "input raster with classes"
 landscape <- terra::unwrap(landscape)
 
 
+
+# create a spatial raster with patch id and LSM values --------------------
+
+
 patches <- get_patches(landscape, to_disk = TRUE)
 
 # this reduction is performed in memory!
-patches <- Reduce(cover, patches$layer_1)
+patches <- setNames(Reduce(cover, patches$layer_1), "id")
 
-# user specified level or based on the beta selection method
+# level should be user specified (e.g based on the beta selection method Andres)
 ms <- spatialize_lsm(landscape, level = "patch", to_disk = TRUE)
+
+# multiband raster
+ms <- rast(ms$layer_1)
+ms2 <- c(patches, ms)
+
+
+# adding info from type to join later with m
+ms3 <-
+    ms2 |>
+    as_tibble() |>
+    pivot_longer(-id, names_to = "function_name", values_to = "value_s") |>
+    left_join(types)
+
+
+
+# create tabular version of LSM  ------------------------------------------
 
 
 m <- calculate_lsm(landscape)
 
-m |> filter(level == "patch")
 
 
+# joining to check if the outputs from calculate_lsm()
+# and spatialize_lsm() are identical
+m |>
+    filter(level == "patch") |>
+    left_join( ms3) |>
+    mutate(check = value == value_s) |>
+    pull(check)
+
+
+# something is wrong. investigation needed.
