af2eda71ff077d5ae73c8304735e02cebb0a496c
Jose Cortes <Cortes-Resendiz@iabg.de>
2024-02-13 09:32:47 +0100

    changes from Andres meeting

--- eo4sdg-landscape-metrics-main.R
+++ eo4sdg-landscape-metrics-main.R
@@ -57,11 +57,11 @@ path <- list(proc_dir = proc_dir, # change it to proc_dir, actually we can delet
              output_files = file.path(out_dir, "output_files")) # change the path to PROBAV_LC100_global_v3.0.1_2015-base_Forest-Type-layer_EPSG-4326.tif
 
 ##### calculate forest landscape metrics ---------------------------------------
-calculate_flm(aoi, lc = path$lc_raster, tempdir = path$proc_dir)
+calculate_flm(aoi, lc = path$lc_raster, tempdir = path$proc_dir, outdir = out_dir)
 path$metrics <- file.path(path$proc_dir, "metrics.csv")
 
 ##### apply selection methods --------------------------------------------------
-calc_beta_rank(df = path$metrics, tempdir = path$proc_dir)
+calc_beta_rank(df = path$metrics, outdir = out_dir)
 path$metrics_ranked <- file.path(path$proc_dir, "metrics_ranked.csv")
 
 # ##### generate maps of the selected metrics ------------------------------------
