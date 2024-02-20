b7a23ec63a31dcf2f7643202c3ee48c652446c6e
Jose Cortes <Cortes-Resendiz@iabg.de>
2024-02-20 13:49:20 +0100

    small changes --Jose

--- eo4sdg-landscape-metrics-main.R
+++ eo4sdg-landscape-metrics-main.R
@@ -11,7 +11,9 @@ library(terra)
 library(rmarkdown)
 library(knitr)
 
+################################################################################
 
+# ONLY RUN IN F-TEP ------------------------------------------------------------
 ### F-TEP ###
 # Getting command line arguments
 # cmd_args <- commandArgs(trailingOnly = TRUE)
@@ -114,21 +116,30 @@ print(files)
 ################################################################################
 
 # Mexico AOI and lc map with 12 forest classes to test locally --JOSE
-# polygon_wkt <- "POLYGON ((-100.389756 20.506008, -100.324451 20.509012, -100.348282 20.557294, -100.388840 20.558581, -100.389756 20.506008))"
-# polygon_wkt_sf <- sf::st_as_sfc(polygon_wkt)
-# aoi <- terra::vect(polygon_wkt_sf)
-# terra::crs(aoi) <- "EPSG:4326"
-# aoi<- sf::st_as_sf(aoi)
-#
-# plot(aoi)
+polygon_wkt <- "POLYGON ((-100.389756 20.506008, -100.324451 20.509012, -100.348282 20.557294, -100.388840 20.558581, -100.389756 20.506008))"
+# polygon_wkt <- "POLYGON((104.77111816406246 8.49138807297875,105.01281738281247 8.53484919469527,105.39733886718746 8.844365054952519,105.53466796874996 8.985461927760653,106.78710937499996 9.65761552211437,106.88598632812496 10.241951639623437,107.67700195312496 10.409481792727007,108.17138671874999 10.819817515515382,109.33593750000001 11.46387438055038,109.57763671874999 12.7957315905158,109.46228027343749 13.670007054279566,109.09973144531251 14.995198836057881,107.4957275390625 14.687224547196834,107.20458984375001 14.213801273022412,107.38037109374999 13.488460431671314,107.40234375 12.567967727957026,106.81457519531247 12.246075604574798,106.21032714843747 11.91304099528864,105.67199707031247 11.64954641194305,105.73791503906247 11.170318336920303,104.39208984374997 10.441896638117186,104.85900878906247 9.86874606765899,104.71069335937497 9.148197713142977,104.73266601562497 8.768367359687133,104.65576171874997 8.616325445300276,104.70520019531246 8.53484919469527,104.77111816406246 8.49138807297875))" #F-TEP failed
 
-########################## LANDSCAPE METRICS ###################################
+polygon_wkt_sf <- sf::st_as_sfc(polygon_wkt)
+aoi <- terra::vect(polygon_wkt_sf)
+terra::crs(aoi) <- "EPSG:4326"
+aoi<- sf::st_as_sf(aoi)
+
+plot(aoi)
+
 
 path <- list(proc_dir = proc_dir, # change it to proc_dir, actually we can delete it
              lc_raster = "//gdfs06/DATEN09/ESA-EO4SDG_D9/01_inputData/openData/raster/PROBAV_LC100/PROBAV_LC100_global_v3.0.1_2015-base_Forest-Type-layer_EPSG-4326.tif",
              output_files = file.path(out_dir, "output_files")) # change the path to PROBAV_LC100_global_v3.0.1_2015-base_Forest-Type-layer_EPSG-4326.tif
 
-# path$lc_raster <- "C:/Users/Cortes-Resendiz/Downloads/PROBAV_LC100_global_v3.0.1_2019_merged.tif"
+path$lc_raster <- "C:/Users/Cortes-Resendiz/Downloads/PROBAV_LC100_global_v3.0.1_2019_merged.tif"
+# path$lc_raster <- "//gdfs06/DATEN09/ESA-EO4SDG_D9/01_inputData/openData/raster/PROBAV_LC100/PROBAV_LC100_global_v3.0.1_2019_Vietnam_merged.tif"
+# error when handling large aoi
+# metrics csv -> names(attributes) mask must be same length as vector
+# Error in names(object) <- nm :
+# 'names' attribute [3] must be the same length as the vector [2]
+
+########################## LANDSCAPE METRICS ###################################
+
 ##### calculate forest landscape metrics ---------------------------------------
 calculate_flm(aoi, lc = path$lc_raster, tempdir = path$proc_dir, outdir = out_dir)
 path$metrics <- file.path(out_dir, "metrics.csv")
@@ -165,8 +176,9 @@ path$metrics_report <- file.path(out_dir, "flm_report.pdf") # output tbd
 # (NOTE: depending on environment objects is NOT good practice.)
 # TODO: Save all needed inputs and load them within the Rmd file.
 
-
+aoi<- terra::vect(aoi)
 render("flm_report.Rmd",
-       output_dir = out_dir)
+       output_dir = out_dir,
+       output_format = "all")
 
 ########################END REPORT#############################################
